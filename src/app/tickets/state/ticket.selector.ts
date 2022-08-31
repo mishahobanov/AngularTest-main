52a9e89eca7f2f674e48db5392f5b679e5851ce0
misha gobanov <m.hobanov@tdz.com.pl>
2022-08-31 17:33:53 +0200

    first commit

---
+++ src/app/tickets/state/ticket.selector.ts
@@ -0,0 +1,11 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { TicketState } from '../ticket.module';
+
+export const getTicketsFeatureState = createFeatureSelector<TicketState>(
+    'tickets'
+)
+
+export const getTickets = createSelector(
+    getTicketsFeatureState,
+    (state: TicketState) => state.tickets.tickets
+)
---
+++ src/app/tickets/ticket-dialog/ticket-dialog.component.ts
@@ -0,0 +1,102 @@
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { createTickets, updateTickets } from '../state/ticket.actions';
+import { TicketState } from '../ticket.module';
+import departments, { ITicket, Ticket } from '../ticket.model';
+
+export interface DialogInsertElement {
+  element: Ticket;
+}
+
+@Component({
+  selector: 'app-ticket-dialog',
+  templateUrl: './ticket-dialog.component.html',
+  styleUrls: ['./ticket-dialog.component.scss'],
+})
+export class TicketDialogComponent  {
+
+  editMode: boolean = false;
+
+  form: FormGroup;
+
+  departmentList = departments;
+
+  editorStyle : {
+    height:'400px',
+  }
+
+  config={
+    toolbar:[
+      ['bold', 'italic', 'underline', 'strike'], 
+      [{ 'header': 1 }, { 'header': 2 }],     
+      ['link'] 
+    ]
+  }
+  constructor(
+    private store: Store<TicketState>,
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<TicketDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public dataDialog: DialogInsertElement
+  ) {
+    if (dataDialog.element) {
+      this.editMode = true;
+    }
+    this.form = this.fb.group({
+      title:  ['', [Validators.required]],
+      department:  ['', [Validators.required]],
+      message: ['', [Validators.required]],
+      attachment: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const obj:Ticket = {
+      ...this.dataDialog.element,
+      ...this.form.value,
+    };
+
+    if (this.editMode) {
+      let ticketEdit : Ticket = {
+        id: obj.id,
+        departmentIdentifier:obj.departmentIdentifier,
+        title:obj.title,
+        message:obj.message,
+        attachment:obj.attachment,
+        status:'Edited',
+        createdAt:obj.createdAt,
+        updateAt:new Date(Date.now())
+      }
+      this.store.dispatch(
+        updateTickets({
+          payload: ticketEdit,
+        })
+      );
+    } else {
+      let ticketCreate: Ticket = {
+        id: "a" + Math.floor(Math.random() * 10) + 1,
+        departmentIdentifier: obj.departmentIdentifier,
+        title: obj.title,
+        message: obj.message,
+        attachment: obj.attachment,
+        status: 'New',
+        createdAt: new Date(Date.now()),
+        updateAt:new Date(Date.now())
+      }
+      this.store.dispatch(
+        createTickets({
+          payload: ticketCreate,
+        })
+      );
+    }
+
+    this.dialogRef.close();
+  }
+  
+}
+
